00e0353a8eaa8d7495f8e8518b5c4160529bfadd
Subham Pradhan <59175226+SGsubuuu-2322@users.noreply.github.com>
2024-01-10 01:19:52 +0530

    Added the general scripts for capturing the HTML elements for further manipulations...

---
+++ script.js
@@ -0,0 +1,5 @@
+const toggle = document.getElementById("toggle");
+const openBtn = document.getElementById("open");
+const closeBtn = document.getElementById("close");
+const modalContainer = document.getElementById("modal");
+ 
