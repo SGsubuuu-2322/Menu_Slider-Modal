329c0516f6b0a1f431dfd8b861971c115d4312d7
Subham Pradhan <59175226+SGsubuuu-2322@users.noreply.github.com>
2024-01-10 01:25:52 +0530

    Added the stylings and scripts for the toggle btn...

--- script.js
+++ script.js
@@ -2,4 +2,7 @@ const toggle = document.getElementById("toggle");
 const openBtn = document.getElementById("open");
 const closeBtn = document.getElementById("close");
 const modalContainer = document.getElementById("modal");
- 
+
+toggle.addEventListener("click", () =>
+  document.body.classList.toggle("show-nav")
+);
